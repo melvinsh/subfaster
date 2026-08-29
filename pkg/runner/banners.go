50e8d3f0e24122824535e7b8fdfb5093144fc24a
Melvin Lammerts <1312973+melvinsh@users.noreply.github.com>
2026-08-29 16:22:47 +0200

    Bump version to v2.20.0

--- pkg/runner/banners.go
+++ pkg/runner/banners.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is the current version of subfaster
-const version = `v2.19.0`
+const version = `v2.20.0`
 
 // GetUpdateCallback returns a callback function that updates subfaster
 func GetUpdateCallback() func() {
